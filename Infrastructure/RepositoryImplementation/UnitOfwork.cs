7daa8d3d41d0af6c3c8a2b683e56134dc2a4c1d4
Boris <borisbesker@gmail.com>
2024-01-29 02:19:15 +0100

    Implemented basic authorization

--- Infrastructure/RepositoryImplementation/UnitOfwork.cs
+++ Infrastructure/RepositoryImplementation/UnitOfwork.cs
@@ -5,15 +5,19 @@ namespace Infrastructure.Repository
     public class UnitOfwork : IUnitOfWork
     {
         private readonly RetailProcurementContext _context;
+
+        //private ISuplierRepository _suplierRepository;
+        private IStoreItemRepository _storeItems;
+
         public UnitOfwork()
         {
             _context = new RetailProcurementContext();
             Supliers = new SuplierRepository(_context);
-            StoreItems = new StoreItemRepository(_context);
+            //StoreItems = new StoreItemRepository(_context);
         }
 
         public ISuplierRepository Supliers { get; private set; }
-        public IStoreItemRepository StoreItems {  get; private set; }
+        public IStoreItemRepository StoreItems  => _storeItems ?? (_storeItems = new StoreItemRepository(_context));
 
         public int Save()
         {
